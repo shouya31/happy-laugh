c4b3fb2e3a96144a957914e9313157963eb9b862
ryumaanai <ryuma.anai@di-v.co.jp>
2021-03-11 21:59:20 +0900

    ビュー崩れを修正

--- template-parts/mobile-new-article.php
+++ template-parts/mobile-new-article.php
@@ -13,8 +13,8 @@
       $authorimg = home_url().$imgurl[2];
     }
   ?>
-  <div class="h-72 w-full flex flex-col items-center p-4">
-    <div class="h-5/6 w-full bg-cover bg-center bg-flower">
+  <div class="h-96 w-full flex flex-col items-center p-4" id="new">
+    <div class="h-2/3 w-full bg-cover bg-center" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
       <div class="w-3/5 flex justify-center bg-white">
         <div class="text-xs text-red-300 pt-2 pb-2">
           <?php if (!is_category() && has_category()): ?>
@@ -28,11 +28,11 @@
         </div>
       </div>
     </div>
-    <div class="h-2/6 w-full text-xl pt-2 pb-2"><?php the_title(); ?></div>
-    <div class="h-1/6 w-full flex flex-col">
-      <div class="w-full text-xs text-gray-300"><?php echo get_the_date('Y/m/d'); ?></div>
+    <div class="h-2/12 w-full text-xl pt-2 pb-2"><?php the_title(); ?></div>
+    <div class="h-3/12 w-full flex flex-col">
+      <div class="text-xs text-gray-300"><?php echo get_the_date('Y/m/d'); ?></div>
       <div class="w-full flex items-center">
-        <div class="h-full w-1/6 bg-contain bg-no-repeat bg-center bg-baby"><?php echo get_avatar( $author ); ?></div>
+        <div class="w-1/6 bg-contain bg-no-repeat bg-center bg-baby"><?php echo get_avatar( $author ); ?></div>
         <div class="text-xs text-gray-300 ml-2"><?php the_author(); ?></div>
       </div>
     </div>
