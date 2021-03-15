7b4263d28c59ab073f9ed57e210e8fbe3731fc55
ryumaanai <ryuma.anai@di-v.co.jp>
2021-03-15 17:43:39 +0900

    RANKINGの記事カラムの構成を変更、表示する数を5に変更

--- template-parts/mobile-ranking-article.php
+++ template-parts/mobile-ranking-article.php
@@ -1,9 +1,14 @@
-<?php
+<div class="px-4 mb-16">
+  <div class="text-lg font-bold border-b-4 border-black pl-2 mb-8">
+    RANKING
+  </div>
+
+  <?php
     $i = 0;
     $tag_posts = get_posts(array(
         'post_type' => 'post', // 投稿タイプ
         'tag'    => 'おすすめ', // タグをスラッグで指定する場合
-        'posts_per_page' => 10, // 表示件数
+        'posts_per_page' => 5, // 表示件数
         'orderby' => 'date', // 表示順の基準
         'order' => 'ASC' // 昇順・降順
     ));
@@ -11,17 +16,18 @@
     if($tag_posts): foreach($tag_posts as $post): setup_postdata($post);
     $i++
   ?>
-  <a href="<?php the_permalink(); ?>" class="w-full flex flex-col items-center px-4 pt-6">
-    <div class="h-36 w-full bg-cover bg-center" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
-      <div class="w-3/5 text-xs text-white text=center py-4 bg-black"><?php echo $i; ?></div>
+  <a href="<?php the_permalink(); ?>" class="h-24 w-full flex px-4 mb-6">
+    <div class="w-1/3 bg-cover bg-center" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
+      <div class="w-1/3 text-xs text-white text-center bg-black py-1"><?php echo $i; ?></div>
     </div>
-    <div class="h-20 w-full text-sm pt-4"><?php the_title(); ?></div>
-    <div class="w-full flex flex-col">
-      <div class="text-xs text-gray-300"><?php echo get_the_date('Y/m/d'); ?></div>
-      <div class="w-full flex items-center">
-        <div class="h-4 w-4 bg-cover bg-no-repeat bg-center"><?php echo get_avatar( $author ); ?></div>
-        <div class="text-xs text-gray-300 ml-2"><?php the_author(); ?></div>
+    <div class="w-2/3 flex flex-col pl-6">
+      <!-- 仮置のカテゴリ表示 -->
+      <div class="flex">
+        <div class="text-xs rounded-full border border-black p-1">category</div>
+        <div class="text-xs rounded-full border border-black p-1">category</div>
       </div>
+      <!-- /仮置のカテゴリ表示 -->
+      <div class="text-sm"><?php the_title(); ?></div>
     </div>
   </a>
   <?php endforeach; endif; wp_reset_postdata(); ?>
