500c628042e29b9d8169000e7e82c55c21b190ec
ryumaanai <ryuma.anai@di-v.co.jp>
2021-03-15 10:55:53 +0900

    RANKINGのフォントサイズ修正

--- template-parts/ranking-article.php
+++ template-parts/ranking-article.php
@@ -24,7 +24,7 @@
         <div class="text-xs text-white pt-2 pb-2"><?php echo $i; ?></div>
       </div>
     </div>
-    <div class="h-full w-2/3 text-xs break-words pl-8 font-verdana"><?php the_title(); ?></div>
+    <div class="h-full w-2/3 text-sm break-words pl-8 font-verdana"><?php the_title(); ?></div>
   </a>
   <?php endforeach; endif; wp_reset_postdata(); ?>
 </div>
