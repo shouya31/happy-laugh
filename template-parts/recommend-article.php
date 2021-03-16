a8ed761e6e885a6427e771764469b95dca970eed
ryumaanai <ryuma.anai@di-v.co.jp>
2021-03-17 01:40:37 +0900

    RECOMMENDの記事カラムのデザインを変更

--- template-parts/recommend-article.php
+++ template-parts/recommend-article.php
@@ -17,28 +17,32 @@
     $i++
   ?>
 
-      <a href="<?php the_permalink(); ?>" class="w-full flex mb-12">
-        <div class="h-56 w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
-          <div class="w-1/2 bg-white text-xs text-center pt-2 pb-2">
-            <?php if (!is_category() && has_category()): ?>
-              <?php
-                $postcat = get_the_category();
-                echo $postcat[0]->name;
-              ?>
-            <?php endif; ?>
-            </div>
-        </div>
-        <div class="h-56 w-2/3 pt-4 pl-8 pb-4">
-          <div class="flex justify-between mb-2">
-            <div class="text-xs text-gray-300"><?php echo get_the_date('Y/m/d'); ?></div>
-            <div class="flex">
-              <div class="h-4 w-4"><?php echo get_avatar( $author ); ?></div>
-              <div class="text-xs text-gray-300 ml-2"><?php the_author(); ?></div>
-            </div>
+    <a href="<?php the_permalink(); ?>" class="w-full flex mb-12">
+      <div class="h-56 w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
+        <div class="w-1/2 bg-white text-xs text-center pt-2 pb-2">
+          <?php if (!is_category() && has_category()): ?>
+            <?php
+              $postcat = get_the_category();
+              echo $postcat[0]->name;
+            ?>
+          <?php endif; ?>
           </div>
-          <div class="h-2/6 text-2xl mb-2"><?php the_title(); ?></div>
-          <div class="h-3/6 text-sm text-gray-400 leading-8"><?php the_excerpt(); ?></div>
+      </div>
+      <div class="h-56 w-2/3 py-1 pl-8">
+        <div class="flex mb-1">
+          <div class="text-xs border-2 border-black rounded-full p-1">カテゴリー</div>
+          <div class="text-xs border-2 border-black rounded-full p-1">カテゴリー</div>
         </div>
-      </a>
+        <div class="flex justify-between mb-1">
+          <div class="text-xs text-gray-300"><?php echo get_the_date('Y/m/d'); ?></div>
+          <div class="flex">
+            <div class="h-4 w-4"><?php echo get_avatar( $author ); ?></div>
+            <div class="text-xs text-gray-300 ml-2"><?php the_author(); ?></div>
+          </div>
+        </div>
+        <div class="h-24 text-2xl mb-1"><?php the_title(); ?></div>
+        <div class="h-16 text-sm text-gray-400"><?php the_excerpt(); ?></div>
+      </div>
+    </a>
   <?php endforeach; endif; wp_reset_postdata(); ?>
 </div>
