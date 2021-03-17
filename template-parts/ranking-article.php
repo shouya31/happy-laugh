6ea2b77b7b05b8b9d9c45ff769b011ad7fcca289
shouya0331 <USER_INFO@users.noreply.github.com>
2021-03-18 00:20:57 +0900

    ランキングliタグ削除

--- template-parts/ranking-article.php
+++ template-parts/ranking-article.php
@@ -4,38 +4,37 @@
   </div>
 
 
-    <ul class="side_menu">
+
+    <?php
+    $i = 0;
+      setPostViews(get_the_ID());
+      query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=10&order=DESC');
+      while(have_posts()) : the_post();
+      $i++;
+    ?>
+
+
+  <a href="<?php the_permalink(); ?>" class="h-24 w-full flex mb-6">
+    <div class="h-full w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
+      <div class="w-1/3 bg-black flex justify-center">
+        <div class="text-xs text-white pt-2 pb-2"><?php echo $i; ?></div>
+      </div>
+    </div>
+    <div class="h-full w-2/3 text-sm break-words pl-8 font-verdana">
+      <div class="flex mb-1">
+        <div class="ranking-category text-xs border border-black rounded-full p-1">コスメ</div>
+      </div>
       <?php
-      $i = 0;
-        setPostViews(get_the_ID());
-        query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=10&order=DESC');
-        while(have_posts()) : the_post();
-        $i++;
+        if(mb_strlen($post->post_title)>30):
+          $title= mb_substr($post->post_title,0,30) ; echo $title. ･･･ ;
+        else:
+          echo $post->post_title;
+        endif;
       ?>
+    </div>
 
-    <li>
-      <a href="<?php the_permalink(); ?>" class="h-24 w-full flex mb-6">
-        <div class="h-full w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
-          <div class="w-1/3 bg-black flex justify-center">
-            <div class="text-xs text-white pt-2 pb-2"><?php echo $i; ?></div>
-          </div>
-        </div>
-        <div class="h-full w-2/3 text-sm break-words pl-8 font-verdana">
-          <div class="flex mb-1">
-            <div class="ranking-category text-xs border border-black rounded-full p-1">コスメ</div>
-          </div>
-          <?php
-            if(mb_strlen($post->post_title)>30):
-              $title= mb_substr($post->post_title,0,30) ; echo $title. ･･･ ;
-            else:
-              echo $post->post_title;
-            endif;
-          ?>
-        </div>
+  </a>
+  <?php endwhile; ?>
+  <?php wp_reset_postdata(); wp_reset_query(); ?>
 
-      </a>
-    </li>
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); wp_reset_query(); ?>
-  </ul>
 </div>
