70d395e404c617bc19f0bb50d5b440a0c2ac81a2
shouya0331 <USER_INFO@users.noreply.github.com>
2021-03-17 22:27:07 +0900

    ランキング機能実装（全カテゴリー含む）

--- template-parts/ranking-article.php
+++ template-parts/ranking-article.php
@@ -3,32 +3,39 @@
     RANKING
   </div>
 
-  <?php
-    $i = 0;
-    $tag_posts = get_posts(array(
-        'post_type' => 'post', // 投稿タイプ
-        'tag'    => 'おすすめ', // タグをスラッグで指定する場合
-        'posts_per_page' => 10, // 表示件数
-        'orderby' => 'date', // 表示順の基準
-        'order' => 'ASC' // 昇順・降順
-    ));
-    global $post;
-    if($tag_posts): foreach($tag_posts as $post): setup_postdata($post);
-    $i++
-  ?>
-  <a href="<?php the_permalink(); ?>" class="h-24 w-full flex mb-6">
-    <div class="h-full w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
-      <div class="w-1/3 bg-black flex justify-center">
-        <div class="text-xs text-white pt-2 pb-2"><?php echo $i; ?></div>
-      </div>
-    </div>
-    <div class="h-full w-2/3 text-sm break-words pl-8 font-verdana">
-      <div class="flex mb-1">
-        <div class="ranking-category text-xs border border-black rounded-full p-1">コスメ</div>
-      </div>
-      <?php the_title(); ?>
-    </div>
 
-  </a>
-  <?php endforeach; endif; wp_reset_postdata(); ?>
+    <ul class="side_menu">
+      <?php
+      $i = 0;
+        setPostViews(get_the_ID());
+        query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=10&order=DESC');
+        while(have_posts()) : the_post();
+        $i++;
+      ?>
+
+    <li>
+      <a href="<?php the_permalink(); ?>" class="h-24 w-full flex mb-6">
+        <div class="h-full w-1/3 bg-cover bg-center bg-no-repeat" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);">
+          <div class="w-1/3 bg-black flex justify-center">
+            <div class="text-xs text-white pt-2 pb-2"><?php echo $i; ?></div>
+          </div>
+        </div>
+        <div class="h-full w-2/3 text-sm break-words pl-8 font-verdana">
+          <div class="flex mb-1">
+            <div class="ranking-category text-xs border border-black rounded-full p-1">コスメ</div>
+          </div>
+          <?php
+            if(mb_strlen($post->post_title)>30):
+              $title= mb_substr($post->post_title,0,30) ; echo $title. ･･･ ;
+            else:
+              echo $post->post_title;
+            endif;
+          ?>
+        </div>
+
+      </a>
+    </li>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); wp_reset_query(); ?>
+  </ul>
 </div>
