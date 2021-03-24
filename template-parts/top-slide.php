2374bac479c30af0b67230e54623715315f81a17
ryumaanai <ryuma.anai@di-v.co.jp>
2021-03-24 16:26:38 +0900

    スライドの丸角をPCのみに

--- template-parts/top-slide.php
+++ template-parts/top-slide.php
@@ -16,10 +16,10 @@
       ?>
         <a
           href="<?php the_permalink(); ?>"
-          class="swiper-slide bg-cover bg-center flex items-end"
+          class="swiper-slide bg-cover bg-center flex items-end sm:rounded-3xl"
           style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>);"
         >
-          <div class="w-full bg-black bg-opacity-75 rounded-b-3xl p-6">
+          <div class="w-full bg-black bg-opacity-75 sm:rounded-b-3xl p-6">
             <div class="text-white mb-4"><?php the_title(); ?></div>
             <div class="flex">
               <div class="text-sm text-white border-2 border-white rounded-full p-2">
